696bfe12ca01d15c43178ed9b434714c94143791
Arpit Kumar <arpitkumar482@gmail.com>
2019-07-23 12:53:48 +0530

    Changes made in regexx.py file by me

--- regexx.py
+++ regexx.py
@@ -3,5 +3,6 @@ import re
 url_list=["https://github.com/arpitkumar482/myappsample.git","https://github.com/arpitkumar482/sample2.git"]
 for url in url_list:
     list_urltags = url.split('/')[-1:][0][:-4]
+    print("after split :"+list_urltags)
     foldername = list_urltags
     print(foldername)
